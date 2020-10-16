4709e275f336934e9b1594b293a0999328996ff8
Joseph Timothy Foley <foley@ru.is>
2020-10-16 20:51:01 +0000

    finished hello_world simple example from 1.2

---
+++ rust-book/01-getting_started/hello_world/main.rs
@@ -0,0 +1,12 @@
+// https://doc.rust-lang.org/stable/book/ch01-02-hello-world.html#writing-and-running-a-rust-program
+// Writing and Running a Rust Program
+//
+//   Compile it:
+//   rustc main.rs
+//
+//   Run it:
+//   ./main
+
+fn main() {
+    println!("Hello, world!");
+}
