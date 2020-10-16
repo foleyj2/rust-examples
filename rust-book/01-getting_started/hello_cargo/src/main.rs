0cd265419da9803bf3b40d4127b5acb423437f64
Joseph Timothy Foley <foley@ru.is>
2020-10-16 21:09:15 +0000

    Annotated the hello_cargo example with cargo commands

---
+++ rust-book/01-getting_started/hello_cargo/src/main.rs
@@ -0,0 +1,12 @@
+// https://doc.rust-lang.org/stable/book/ch01-03-hello-cargo.html
+// Building and Running a Cargo Project
+
+// useful cargo commands
+//   cargo build   #compile it
+//   cargo run     #compile (if needed) and run it
+//   cargo check   #just see if there are any errors/warnings but don't
+//                 #make an executable
+
+fn main() {
+    println!("Hello, world!");
+}
